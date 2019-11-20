ec11a92fb3b19e75eca4e4583021c6fa4a7e47d4
Sid <sid@Sids-MacBook-Pro.local>
2019-11-19 20:07:45 -0500

    conflict management (updated w/ comments)

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
-
+//Sets the date picker to today's date and the time picker to right now
 function setDateandTime() {
     let date = new Date();
 
     let day = date.getDate();
 
-    let month = date.getMonth() + 1;
+    let month = date.getMonth() + 1;    //+1 because 0-Jan, 1-Feb, 2-Mar,etc.
     let year = date.getFullYear();
     let hour = date.getHours();
     let min  = date.getMinutes();
 
+    //Formatting to appease the date input and time input standards
     month = (month < 10 ? "0" : "") + month;
     day = (day < 10 ? "0" : "") + day;
     hour = (hour < 10 ? "0" : "") + hour;
@@ -17,12 +18,14 @@ function setDateandTime() {
     let today = year + "-" + month + "-" + day;
     let displayTime = hour + ":" + min;
 
+    //Updating the HTML
     $('#datePicker').val(today);
     $('#timePicker').val(displayTime);
 }
 
+//Updates the displayed Rec Center population based on the Date and Time selected by getting data from that date and time from the database
 function getNumber() {
-
+    //Extracts the time from the timePicker
     let time = $('#timePicker').val();
     let hours = parseInt(time.substring(0,2));
     let minutes = parseInt(time.substring(3));
@@ -30,15 +33,17 @@ function getNumber() {
     if(minutes >= 30){
         hours += 1;
     }
-
+    //Since there is no data from 1am to 4am, default to the latest available time (00:00)
     if(hours > 0 && hours < 5){
         hours = 0;
     }
 
     let date = new Date($('#datePicker').val())
 
+    //ready the payload for HTTP Request
     let payload = {day:date.getUTCDay(), time: hours};
 
+    //Deploy payload
     $.ajax({
         type: 'GET',
         url: '/api/getCrowd',
@@ -94,9 +99,13 @@ function showSports(elem){
   }
 }
 
+//Once the DOM is loaded
 $().ready( function() {
+    //Run these functions
     setDateandTime();
     getNumber();
     popPartySize();
+    
+    //Add event listeners
     $('#refresh').click(getNumber);
 })
